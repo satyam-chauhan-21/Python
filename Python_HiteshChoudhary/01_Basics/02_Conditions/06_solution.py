361c350625c5ee47b369cf9a15651e40ab645799
satyam-chauhan-21 <archauhan4390@gmail.com>
2024-03-03 15:40:12 +0530

    added 06_solution

---
+++ Python_HiteshChoudhary/01_Basics/02_Conditions/06_solution.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------#
+#                           5. Weather Activity Suggestion
+#-------------------------------------------------------------------------------#
+# Problem: Choose a mode of transportation based on the distance 
+# (e.g., <3 km: Walk, 3-15 km: Bike, >15 km: Car).
+
+distance = int(input("enter distance in KM : "))
+
+if distance < 3:
+    transport = "Walk"
+elif distance <= 15:
+    transport = "Bike"
+else:
+    transport = "Car"
+
+print(transport)
