5ea13ce887e1efe15fcf4d9e0354f41687ad21d0
satyam-chauhan-21 <archauhan4390@gmail.com>
2024-03-03 16:02:08 +0530

    added 07_solution

--- Python_HiteshChoudhary/01_Basics/02_Conditions/07_solution.py
+++ Python_HiteshChoudhary/01_Basics/02_Conditions/07_solution.py
@@ -2,3 +2,16 @@
 #                           7. Coffee Customization
 #-------------------------------------------------------------------------------#
 # Problem: Customize a coffee order: "Small", "Medium", or "Large" with an option for "Extra shot" of espresso.
+
+coffee_size = input("Coffee size 1.Small 2.Meadium 3.Large : ")
+extra_short = input("Extra short yes or no : ").lower()
+
+if extra_short == "yes":
+    coffee = coffee_size + " coffee with an extra short"
+elif extra_short == "no":
+    coffee = coffee_size + " coffee" 
+else:
+    print("please enter only Yes or No")
+    exit()
+
+print("Order :", coffee)
