1b6785f486cd5b035e35e7206de6ff798d2d9ad0
satyam-chauhan-21 <archauhan4390@gmail.com>
2024-03-03 15:34:50 +0530

    added 05_solution with a tip

--- Python_HiteshChoudhary/01_Basics/02_Conditions/05_solution.py
+++ Python_HiteshChoudhary/01_Basics/02_Conditions/05_solution.py
@@ -4,6 +4,7 @@
 # Problem: Suggest an activity based on the weather 
 # (e.g., Sunny - Go for a walk, Rainy - Read a book, Snowy - Build a snowman).
 
+# tip
 # here i use .lower() at the end in case user enter capitalized value it will converted in lower case.
 # and this will usefull because in "if" constion we use lower case values to check condition.
 weather = input("Enter Weather 1.Sunny 2.Rainy 3.Snowy : ").lower()
