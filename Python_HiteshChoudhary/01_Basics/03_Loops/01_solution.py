12f8a87c63a9c7f704b710cd2e5fa3363c3c23a1
satyam-chauhan-21 <archauhan4390@gmail.com>
2024-03-06 14:12:02 +0530

    added 01_solution of 03_Loops.

---
+++ Python_HiteshChoudhary/01_Basics/03_Loops/01_solution.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------#
+#                         1. Age Group Categorization
+#-------------------------------------------------------------------------------#
+#Problem: Given a list of numbers, count how many are positive.
+
+numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+count = 0
+
+for i in numbers:
+    if i > 0:
+        print(i, end = " ")
+        count = count + 1
+
+print("\nso total",count,"positive number in given\n")
