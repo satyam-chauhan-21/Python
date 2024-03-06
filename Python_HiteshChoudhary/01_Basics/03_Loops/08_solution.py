eb2d898038d5aaedb4cc573b574d25493c167095
satyam-chauhan-21 <archauhan4390@gmail.com>
2024-03-06 21:21:14 +0530

    modified 08_solution of Loops

--- Python_HiteshChoudhary/01_Basics/03_Loops/08_solution.py
+++ Python_HiteshChoudhary/01_Basics/03_Loops/08_solution.py
@@ -6,9 +6,11 @@
 number = int(input("enter number to check prime : "))
 is_prime = True
 
-for i in range(2,number):
-    if number % i == 0:
-         is_prime = False
+if number > 1:
+     for i in range(2,number):
+          if number % i == 0:
+               is_prime = False
+               break
 
 if is_prime:
      print(number, "is Prime.")
